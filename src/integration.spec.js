d5f742a3059b1528740b62769f0ae5c3897efee6
traynor <10799583+traynor@users.noreply.github.com>
2018-01-14 10:16:51 +0100

    test(integration): setup integration tests;

---
+++ src/integration.spec.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+
+describe('Module', function() {
+
+  describe('should take proper config arguments for: ', function() {
+
+    it.skip('server', function() {
+
+    });
+    it.skip('auth', function() {
+
+    });
+    it.skip('logger', function() {
+
+    });
+  });
+
+  describe('when serving static files with auth and logger', function() {
+
+    it.skip('should have HTTP server running', function() {
+
+    });
+    it.skip('should have basic auth guard', function() {
+
+    });
+    it.skip('should have access logged to a file', function() {
+
+    });
+  });
+
+});
