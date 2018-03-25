6bbbca900d6de6354fc26b04219f9e9f02870f5b
traynor <10799583+traynor@users.noreply.github.com>
2018-03-25 10:26:09 +0200

    chore(tests): stage test file;

---
+++ src/config-disabled.spec.js
@@ -0,0 +1,102 @@
+import assert from 'better-assert';
+import defaultConfig from './server/default-config';
+import fs from 'fs';
+import nrc from 'node-run-cmd';
+import request from 'superagent';
+
+let config, inst;
+
+//const key = fs.readFileSync(__dirname + '/../' + defaultConfig.server.ssl.key);
+const cert = fs.readFileSync(__dirname + '/../' + defaultConfig.server.ssl.cert);
+
+config = {
+  nodeStatic: {
+    // all available node-static options https://www.npmjs.com/package/node-static: `new static.Server(root, options)`
+    // use path relative to project root, i.e. process.cwd()
+    root: 'example/public',
+    options: {
+      indexFile: 'index.html' || 'index.html'
+    }
+  },
+  // our web server options
+  server: {
+    port: 4012,
+    http2: false,
+    ssl: {
+      enabled: true,
+      httpListener: 4010,
+      // enter path relative to project root
+      key: 'example/server/localhost-test-privkey.pem',
+      cert: 'example/server/localhost-test-cert.pem'
+    },
+    customPages: {
+      forbidden: 'forbidden.html',
+      notFound: 'notFound.html',
+      error: 'error.html'
+    }
+  },
+  // basic auth credentials
+  auth: {
+    enabled: false, // false disable
+    name: 'test' || process.env.NAME,
+    pass: 'test' || process.env.PASS,
+    realm: 'Private' || process.env.REALM
+  },
+  // logger file options
+  // todo: enable morgan conf
+  logger: {
+    use: false, // false disable
+    // make sure directory exists first
+    filename: 'disabled-access.log',
+    folder: 'example/server/dlogs',
+    type: 'combined',
+    options: {}
+  }
+}
+
+
+before(function(done) {
+  // runs before all tests in this block
+
+  const NodeStaticAuth3 = require('../lib');
+
+  let disabled = new NodeStaticAuth3(config, (svr) => {
+    inst = svr;
+    done();
+  });
+});
+
+
+describe('static-auth server', function() {
+
+
+  it('should get with logger and auth disabled', function(done) {
+
+    request
+      .get(`http://localhost:${config.server.ssl.httpListener}/?redirect-2-secured`)
+      .ca(cert)
+      .end(function(err, res) {
+        assert(res.ok);
+        done();
+      });
+  });
+
+  it('should get custom 404 page', function(done) {
+
+    request
+      .get(`${config.server.ssl.enabled ? 'https://' : 'http://'}localhost:${config.server.port}/no-page-here`)
+      .ca(cert)
+      .end(function(err, res) {
+        assert(res.status === 404);
+        assert(res.text.includes(`<h1>404</h1>`));
+        done();
+      });
+  });
+
+});
+
+after(function(done) {
+
+  inst.close();
+  done();
+})
